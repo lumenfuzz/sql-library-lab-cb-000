232872b290acf3e22451b60a928f8f5a73a1f215
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-17 20:32:23 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title FROM series INNER JOIN characters ON Characters.series_id = Series.id GROUP BY species ORDER BY species DESC LIMIT 1"
+  "SELECT title FROM series INNER JOIN characters ON Characters.series_id = Series.id GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
